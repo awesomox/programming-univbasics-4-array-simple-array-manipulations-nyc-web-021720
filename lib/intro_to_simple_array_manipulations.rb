0a5ba36b19c753275f20dcf5650347c750f7391c
tom hannaford <thom.hannaford@gmail.com>
2019-12-25 18:40:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,7 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-  array.pop(-2, 2)
+  array.pop(2)
 end
 
 def using_shift(array)
@@ -21,7 +21,7 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
-  array.shift(0, 2)
+  array.shift(2)
 end
 
 def using_concat(array1, array2)
