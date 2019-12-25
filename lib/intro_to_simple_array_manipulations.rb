089334c5acc56ef8a06931699e3c8767a8c2cea7
tom hannaford <thom.hannaford@gmail.com>
2019-12-25 18:27:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+nyc_bands = ["Wu-Tang", "Ramones", "MOP"]
+
+def using_push(array, element)
+  array.push(element)
+end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  array.pop[]
